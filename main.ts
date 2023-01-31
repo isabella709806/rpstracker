e00c1218922a5a7ba44390ec8e24e0c0485e9ab3
isabella709806 <122543587+isabella709806@users.noreply.github.com>
2023-01-31 12:52:57 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+input.onGesture(Gesture.Shake, function () {
+    reset()
+})
 function updateScores () {
     OLED.clear()
     OLED.writeStringNewLine("player 1" + p1)
@@ -8,15 +11,18 @@ function updateScores () {
     OLED.newLine()
     OLED.writeStringNewLine("rounds" + rounds)
 }
-let p1 = 0
-let p2 = 0
+function reset () {
+    OLED.init(128, 64)
+    OLED.writeStringNewLine("Shall we play a game?")
+    ties = 0
+    rounds = 0
+    p2 = 0
+    p1 = 0
+    basic.pause(2000)
+    updateScores()
+}
 let rounds = 0
 let ties = 0
-OLED.init(128, 64)
-OLED.writeStringNewLine("Shall we play a game?")
-ties = 0
-rounds = 0
-p2 = 0
-p1 = 0
-basic.pause(2000)
-updateScores()
+let p2 = 0
+let p1 = 0
+reset()
